987d0d30cc1c186b2ff8a411ea5f417ba08c05b2
AVMihel <beglec2010@gmail.com>
2025-04-16 12:59:08 +0500

    Complete the second task

--- src/test/java/ru/netology/rest/RegistrationTest.java
+++ src/test/java/ru/netology/rest/RegistrationTest.java
@@ -1,7 +1,6 @@
 package ru.netology.rest;
 
 import com.codeborne.selenide.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -21,7 +20,7 @@ class RegistrationTest {
 
 
     @Test
-    void shouldBeReturnedFormCcompletedSuccessfully() {
+    void shouldBeReturnedFormCompletedSuccessfully() {
         Selenide.open("http://localhost:9999");
 
         //Поле "Город"
@@ -34,6 +33,44 @@ class RegistrationTest {
         $("[data-test-id='date'] input")
                 .sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='date'] input").shouldHave(value(planningDate));
+
+        //Поле "Фамилия и имя"
+        $("[data-test-id='name'] input").setValue("Синицин Евгений");
+        $("[data-test-id='name'] .input__control")
+                .shouldHave(value("Синицин Евгений"));
+
+        //Поле "Мобильный телефон"
+        $("[data-test-id='phone'] input").setValue("+79397246154");
+        $("[data-test-id='phone'] .input__control")
+                .shouldHave(value("+79397246154"));
+
+        //Чек-бокс
+        $("[data-test-id='agreement']").click();
+
+        //Кнопка
+        $("button.button_view_extra").click();
+
+        //Проверка успешной отправки
+        $(Selectors.withText("Успешно!"))
+                .should(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldBeReturnedFormCompletedSuccessfullyComplexElement() {
+        Selenide.open("http://localhost:9999");
+
+        //Поле "Город"
+        $("[data-test-id='city'] input").setValue("Сал");
+        $$(".menu-item__control").findBy(text("Салехард")).click();
+        $("[data-test-id='city'] .input__control")
+                .shouldHave(value("Салехард"));
+
+        //Поле "Дата встречи"
+        $("[data-test-id='date'] input").click();
+        String planningDate = generateDate(7, "dd.MM.yyyy");
+        String day = generateDate(7, "d");
+        $$(".calendar__day").findBy(text(day)).click();
         $("[data-test-id='date'] input").shouldHave(value(planningDate));
 
         //Поле "Фамилия и имя"
